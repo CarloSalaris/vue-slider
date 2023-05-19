6d04777156b53f5c3552d305e99939204b24d2d5
CarloSalaris <carloa.salaris@gmail.com>
2023-05-19 16:57:40 +0200

    Bonus 1 if click on thumbnail it shows in wide preview

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 /* Descrizione:
 Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue. */
 
+/* Bonus:
+1- al click su una thumb, visualizzare in grande l’immagine corrispondente
+2- applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+3- quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce */
+
 const{createApp} = Vue
 createApp({
     data(){
