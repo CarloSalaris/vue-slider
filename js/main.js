b7eede510e0ec008f7a9d70cdb4756fff8f72329
CarloSalaris <carloa.salaris@gmail.com>
2023-05-19 16:12:16 +0200

    Images in DOM with Vue

---
+++ js/main.js
@@ -0,0 +1,43 @@
+/* Descrizione:
+Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue. */
+
+const{createApp} = Vue
+createApp({
+    data(){
+        return {
+            activePosition: 2,
+            movies: [
+                {
+                    image: 'img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morales',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                }, {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                }, {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
+            ]
+        }
+    },
+    methods: {
+        methodName() {
+            //code
+        }
+    },
+    mounted() {
+	  //code
+        console.log("ho caricato l'app");
+    }
+}).mount("#app")
